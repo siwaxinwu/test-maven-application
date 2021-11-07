c7e866bce1ef68652e68647f0841a04aa69ac910
dingyawu <yawu.ding@ximalaya.com>
2021-11-07 12:48:54 +0800

    代码提交

---
+++ src/main/java/testidea/Testdebug.java
@@ -0,0 +1,28 @@
+package testidea;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * @description:
+ * @author: Ding Yawu
+ * @create: 2021/11/7 11:59
+ */
+public class Testdebug {
+    public static void main(String[] args) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (isInterested(random.nextInt(10))) {
+                count++;
+            }
+        }
+        System.out.printf("Found %d interested values%n", count);
+    }
+
+
+
+    private static boolean isInterested(int i) {
+        return i % 2 == 0;
+    }
+
+}
